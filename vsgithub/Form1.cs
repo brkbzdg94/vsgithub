888dcf4712ff5578e5a9d26e87cd6112e4a3f42a
Burak <cixnx@hotmail.com>
2018-09-18 00:24:17 +0300

    new button

--- vsgithub/Form1.cs
+++ vsgithub/Form1.cs
@@ -21,5 +21,15 @@ namespace vsgithub
         {
             MessageBox.Show("Merhaba Dünya");
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Güle Güle");
+        }
     }
 }
