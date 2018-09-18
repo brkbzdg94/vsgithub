10dc6d40d7837c55ca95406982b8b1c12b104c47
Burak <cixnx@hotmail.com>
2018-09-18 17:49:54 +0300

    yeni kural 2

--- vsgithub/Form1.cs
+++ vsgithub/Form1.cs
@@ -24,11 +24,18 @@ namespace vsgithub
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int target = -5;
-            int num = 3;
+            //int target = -5;
+            //int num = 3;
 
-            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
-            target = +num; // Noncompliant; target = 3
+            //target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            //target = +num; // Noncompliant; target = 3
+
+
+            var s = "foo";
+            var t = "fee fie foe " + s.ToString();  // Noncompliant
+            var someObject = new object();
+            var u = "" + someObject.ToString(); // Noncompliant
+            var v = string.Format("{0}", someObject.ToString()); // Noncompliant
         }
 
         private void button2_Click(object sender, EventArgs e)
